a642a302688817d1df137a9e6d170670afca77fa
Ben Morgan <bmorgan.warwick@gmail.com>
2013-03-04 12:58:09 +0000

    Add basic main program
    Provide skeleton main program for micro_pipeline and CMake script to
    build a command line executable.

---
+++ Source/micro_pipeline.cpp
@@ -0,0 +1,20 @@
+// micro_pipeline - Main program for MicroPipeline
+//
+// Copyright (c) 2013 by Ben Morgan <bmorgan.warwick@gmail.com> 
+// Copyright (c) 2013 by The University of Warwick
+//
+// Distributed under the OSI-approved BSD 3-Clause License (the "License");
+// see accompanying file License.txt for details.
+//
+// This software is distributed WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the License for more information.
+
+
+#include <cstdlib>
+
+int main(int argc, const char *argv[]) {
+  
+  return EXIT_SUCCESS;
+}
+
